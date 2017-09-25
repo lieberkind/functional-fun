6449981af63e3717d8dfd8c39b94cce2966714bd
Tomas Lieberkind <t.lieberkind@gmail.com>
2017-09-25 15:48:00 +0200

    updated functions with comments

--- curry.js
+++ curry.js
@@ -1,13 +1,19 @@
 const curry = fn => {
+	// concatenates arguments until it matches or exceeds the arity of the
+	// original function. At that point in time, call the original function
 	const _curry = args => {
 		return args.length >= fn.length
 			? fn(...args)
 			: (...otherArgs) => _curry([...args, ...otherArgs]);
 	};
 
+	// if the original function does not take any arguments, just return it
 	return fn.length === 0 ? fn : _curry([]);
 };
 
+// works in the same way as curry, but without using an inner function. Uses
+// recursion more elegantly (in my opinion), but exposes a more bloated
+// interface
 const curry2 = (fn, args = []) => {
 	if (fn.length === 0) {
 		return fn;
